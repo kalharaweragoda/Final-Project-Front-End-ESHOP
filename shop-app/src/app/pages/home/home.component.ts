39ad5de2c005e52555ee986d9b4965a6e5eb8850
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-08 10:52:34 +0530

    create home.component.html page

---
+++ shop-app/src/app/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { env } from '../../env/env.test';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent{
+
+ 
+
+
+}
