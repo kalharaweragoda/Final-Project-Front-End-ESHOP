cfd390b72b37790dd4ff7e41b59e996bd352e0bd
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-08 11:18:35 +0530

    change app.routes.ts file

--- shop-app/src/app/app.routes.ts
+++ shop-app/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { AddCustomerComponent } from './pages/add-customer/add-customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+    children: [
+      {
+        path: 'customer',
+        component: AddCustomerComponent,
+      },
+    ],
+  },
+];
