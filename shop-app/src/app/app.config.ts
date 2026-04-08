183b6b965a0ccf24997b56838070f055a5dadffb
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-08 11:19:25 +0530

    change app.config.ts file

--- shop-app/src/app/app.config.ts
+++ shop-app/src/app/app.config.ts
@@ -1,11 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme:{
+        preset: Aura
+      }
+    })
   ]
 };
