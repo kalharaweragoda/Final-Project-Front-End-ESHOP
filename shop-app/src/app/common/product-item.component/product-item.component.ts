f316d9676ded19d20f0cf4ba23885fb8c05ac352
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-08 10:08:43 +0530

    rename navbar name file

---
+++ shop-app/src/app/common/product-item.component/product-item.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-item.component',
+  imports: [],
+  templateUrl: './product-item.component.html',
+  styleUrl: './product-item.component.css',
+})
+export class ProductItemComponent {}
