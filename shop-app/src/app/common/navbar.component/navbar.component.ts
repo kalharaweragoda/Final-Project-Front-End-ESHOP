c87b4478ec860594eab40a8c1428be8d79a0050d
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-08 10:05:33 +0530

    create navbar.component.ts file

--- shop-app/src/app/common/navbar.component/navbar.component.ts
+++ shop-app/src/app/common/navbar.component/navbar.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
-  selector: 'app-navbar.component',
-  imports: [],
+  selector: 'app-navbar',
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css',
+  styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {}
+export class NavbarComponent{
+
+}
