40e7a82ee9a6ce82ca686fd950e2ec56c5bed794
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-08 10:58:15 +0530

    create product.component.ts page

--- shop-app/src/app/pages/products/products.component.ts
+++ shop-app/src/app/pages/products/products.component.ts
@@ -14,6 +14,45 @@ import { ProductService } from '../../service/ProductService';
 export class ProductsComponent implements OnInit {
   constructor(private productService:ProductService){}
 
- 
+  filters = ['price', 'rating'];
+  order = ['ascending', 'descending'];
+  selectedFilter: string = '';
+  selectedOrder: string = '';
+
+  ngOnInit(): void {
+    this.loadProductInfo();
+  }
+
+  loading: boolean = false;
+  public listOfProducts: any = [];
+
+  loadProductInfo() {
+    this.productService.getAll().subscribe({
+      next: (data:Product[]) => {
+        this.listOfProducts = data;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = true;
+      },
+    });
+  }
+
+  onFilterChange(): void {
+    if (!this.selectedFilter || !this.selectedOrder) return;
+
+    const direction = this.selectedOrder === 'ascending' ? 1 : -1;
+
+    this.listOfProducts.sort((a: Product, b: Product) => {
+      switch (this.selectedFilter) {
+        case 'price':
+          return (a.price - b.price) * direction;
+        case 'rating':
+          return (a.rating.rate - b.rating.rate) * direction;
+
+        default:
+          return 0;
+      }
+    });
   }
 }
