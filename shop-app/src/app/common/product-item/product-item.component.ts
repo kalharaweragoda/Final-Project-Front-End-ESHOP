9040b7b90d3a1c5001ee03b0c47762c4c66bbe66
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-08 10:12:45 +0530

    create productcomponent.ts files

--- shop-app/src/app/common/product-item/product-item.component.ts
+++ shop-app/src/app/common/product-item/product-item.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, Input } from '@angular/core';
+import { CartService } from '../../service/CartService';
+import { Product } from '../../models/Product';
+import Swal from 'sweetalert2';
+
 
 @Component({
-  selector: 'app-product-item.component',
-  imports: [],
+  selector: 'app-product-item',
+  imports: [CommonModule],
   templateUrl: './product-item.component.html',
-  styleUrl: './product-item.component.css',
+  styleUrl: './product-item.component.css'
 })
-export class ProductItemComponent {}
+export class ProductItemComponent {
+  @Input()
+  public item:any;
+
+  cartService = inject(CartService);
+
+
+  addToCart(item: Product) {
+    const added = this.cartService.addToCart(item);
+    if (added) {
+      Swal.fire('Success!','Added to cart!','success');
+    } else {
+      Swal.fire('Oops!','Item already in cart!', 'info');
+    }
+  }
+}
