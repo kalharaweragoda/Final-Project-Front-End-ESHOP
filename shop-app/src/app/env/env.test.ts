9d9bc45443587282ea5b7e44d03bef1a787af3a8
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-08 10:20:30 +0530

    create env.test.ts file

---
+++ shop-app/src/app/env/env.test.ts
@@ -0,0 +1,7 @@
+export const env = {
+  UNSPLASH_ROOT: 'https://api.unsplash.com',
+  ACCESS_KEY: 'Bz3rWDhLCHLIdRZYhe1TcCoKwSzTXNwdjOnLtRGUkZ0',
+  SECRET_KEY: 'eIyyDdFjTKGOflRnZ8eEdGvL2K1zzysw3mX-vee2-x8',
+  PRODUCT_SERVICE_URL : 'http://localhost:8081',
+  ORDER_SERVICE_URL : 'http://localhost:8080'
+};
