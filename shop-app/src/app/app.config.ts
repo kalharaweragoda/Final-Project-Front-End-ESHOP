621b7f04cb9b18c584ed7683ee2bace4a86a02b0
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-08 14:23:34 +0530

    files change

--- shop-app/src/app/app.config.ts
+++ shop-app/src/app/app.config.ts
@@ -7,13 +7,10 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),
     provideAnimationsAsync(),
     providePrimeNG({
-      theme: {
+      theme:{
         preset: Aura
       }
     })
--- shop-app/src/main.ts
+++ shop-app/src/main.ts
@@ -1,8 +1,3 @@
-import 'zone.js'; // Zone.js අතින්ම load කිරීම
-
-// Global Error එක සඳහා විසඳුම
-(window as any).global = window;
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
