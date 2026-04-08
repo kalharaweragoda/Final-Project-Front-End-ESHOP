a5e08a46557b84ddc1922f9a30fbe31db55daf20
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-08 10:43:31 +0530

    added checkout-model pages

---
+++ shop-app/src/app/pages/checkout-modal/checkout-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout-modal',
+  imports: [FormsModule],
+  templateUrl: './checkout-modal.component.html',
+  styleUrl: './checkout-modal.component.css'
+})
+export class CheckoutModalComponent {
+
+}
